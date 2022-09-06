8685c90afc8074914f6a5db2cefb616ca68e56cd
roberto <roberto@Tannhaus.localdomain>
2022-09-06 17:22:47 +0700

    lab_3

---
+++ src/lab_3/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define NUMBER_OF_THREADS 4
+#define NUMBER_OF_LINE 2
+
+void* print_lines(void* argv);
+
+int main()
+{
+	pthread_t pthread_array[NUMBER_OF_THREADS];
+
+    const char* const TEXT[NUMBER_OF_THREADS] = { 
+                    "first thread",
+                    "second thread", 
+                    "third thread",
+                    "fourth thread"
+                };
+
+    for (int i = 0; i < NUMBER_OF_THREADS; ++i)
+    {
+        if (0 != pthread_create(&pthread_array[i], NULL, print_lines, (void*)TEXT[i]))
+        {
+            perror("Failed to create a thread");
+            return EXIT_FAILURE;
+        }
+    } 
+
+    for (int i = 0; i < NUMBER_OF_THREADS; ++i)
+    {
+        if (0 != pthread_join(pthread_array[i], NULL))
+        {
+            perror("Failed to join the thread");
+            return EXIT_FAILURE;
+        }
+    }
+
+	return EXIT_SUCCESS;
+}
+
+void* print_lines(void* argv)
+{
+	if (NULL == argv)
+	{
+		fprintf(stderr, "Wrong arguments for 'print_lines()'\n");
+		exit(EXIT_FAILURE);
+	}
+
+	const char* const line_to_print = (const char* const) argv;
+
+	for (int i = 0; i < NUMBER_OF_LINE; ++i)
+	{
+		printf("Written by %s - %d\n", line_to_print, i);
+	}
+
+	return NULL;
+}
+
