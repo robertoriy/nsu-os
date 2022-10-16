9442c6fb4c3ff89280663dc9bde22c5aa0518f45
unknownRobert <daniil.lyutaev@gmail.com>
2022-10-16 23:46:55 +0700

    moved error_check to header

--- src/lab_8/main.c
+++ src/lab_8/main.c
@@ -2,32 +2,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include "error_check.h"
 
 #define NUMBER_OF_STEPS 2000000000
-#define CHECK_FUNCTION(function, action_on_error) do \
-        { \
-            int return_value = (function); \
-            if (EXIT_SUCCESS != return_value) { \
-                fprintf(stderr, "Runtime error: %s returned %d at %s:%d\n", #function, return_value, __FILE__, __LINE__); \
-                action_on_error; \
-            } \
-        } while (0)
-
-#define CHECK_THREAD_ARGUMENTS(return_value, action_on_error) do \
-        { \
-            if ((void*)EXIT_FAILURE == return_value) { \
-                fprintf(stderr, "Runtime error: Thread have got wrong arguments. Error at %s:%d\n", __FILE__, __LINE__); \
-                action_on_error; \
-            } \
-        } while (0)
-
-#define CHECK_NULL(value, action_on_error) do\
-        { \
-            if (NULL == value) { \
-                fprintf(stderr, "Runtime error: Unexpected NULL occured at %s:%d\n", __FILE__, __LINE__); \
-                action_on_error; \
-            } \
-        } while (0)
 
 typedef struct thread_data
 {
