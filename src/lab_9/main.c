4b04673d878dbe2ffe684debc04b85e73f489131
unknownRobert <daniil.lyutaev@gmail.com>
2022-10-17 03:58:59 +0700

    lab_9 are ready for class

--- src/lab_9/main.c
+++ src/lab_9/main.c
@@ -1,45 +1,31 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <string.h>
+#include "error_check.h"
 
-#define NUMBER_OF_STEPS 2000000000
-#define CHECK_FUNCTION(function, action_on_error) do \
-        { \
-            int return_value = (function); \
-            if (EXIT_SUCCESS != return_value) { \
-                fprintf(stderr, "Runtime error: %s returned %d at %s:%d\n", #function, return_value, __FILE__, __LINE__); \
-                action_on_error; \
-            } \
-        } while (0)
+#define CHECK_STEP 1000000
 
-#define CHECK_THREAD_ARGUMENTS(return_value, action_on_error) do \
-        { \
-            if ((void*)EXIT_FAILURE == return_value) { \
-                fprintf(stderr, "Runtime error: Thread have got wrong arguments. Error at %s:%d\n", __FILE__, __LINE__); \
-                action_on_error; \
-            } \
-        } while (0)
-
-#define CHECK_NULL(value, action_on_error) do\
-        { \
-            if (NULL == value) { \
-                fprintf(stderr, "Runtime error: Unexpected NULL occured at %s:%d\n", __FILE__, __LINE__); \
-                action_on_error; \
-            } \
-        } while (0)
+int NUMBER_OF_THREADS = 0;
+long long MAX_ITERATION = CHECK_STEP;
+pthread_mutex_t mutex;
+bool exit_flag = false;
 
 typedef struct thread_data
 {
     long long start;
-    long long number_of_iterations;
     double local_result;
 } thread_data;
 
-
 void* pi_counter(void* argv);
-void fill_displacements_data(const int NUMBER_OF_THREADS, thread_data* const displacements);
-int memory_allocation(const int NUMBER_OF_THREADS, pthread_t** pthreads, thread_data** displacements);
+void count_to_the_end(long long start_multiplier, long long number_of_iterations, double* result);
+int set_sigint_handler();
+void signal_handler(int signal);
+int memory_allocation(pthread_t** pthreads, thread_data** thread_info);
+void destroy_all_elements(pthread_mutex_t mutex, pthread_t* pthreads, thread_data* thread_info);
 
 int main(int argc, char** argv)
 {
@@ -48,88 +34,125 @@ int main(int argc, char** argv)
         fprintf(stderr, "Expected one argument\n");
         return EXIT_FAILURE;
     }
-
-    int EXPECTED_NUMBER_OF_THREADS = atoi(argv[1]);
-    int ACTUAL_NUMBER_OF_THREADS = 0;
-
-    if (EXPECTED_NUMBER_OF_THREADS < 1)
+    NUMBER_OF_THREADS = atoi(argv[1]);
+    if (NUMBER_OF_THREADS < 1)
     {
         fprintf(stderr, "Expected value >= 1\n");
         return EXIT_FAILURE;
     }
 
     pthread_t* pthreads;
-    thread_data* displacements;
+    thread_data* thread_info;
+    CHECK_FUNCTION(memory_allocation(&pthreads, &thread_info), return EXIT_FAILURE);
 
-    CHECK_FUNCTION(memory_allocation(EXPECTED_NUMBER_OF_THREADS, &pthreads, &displacements), return EXIT_FAILURE);
+    CHECK_FUNCTION(pthread_mutex_init(&mutex, NULL), free(pthreads); free(thread_info); return EXIT_FAILURE);
 
-    fill_displacements_data(EXPECTED_NUMBER_OF_THREADS, displacements);
+    CHECK_FUNCTION(set_sigint_handler(), destroy_all_elements(mutex, pthreads, thread_info); return EXIT_FAILURE);
 
-    for (int i = 0; i < EXPECTED_NUMBER_OF_THREADS; ++i)
+    for (int i = 0; i < NUMBER_OF_THREADS; ++i)
     {
-        CHECK_FUNCTION(pthread_create(&pthreads[i], NULL, pi_counter, (void*) &displacements[i]), break);
-        ++ACTUAL_NUMBER_OF_THREADS;
+        thread_info[i].start = i;
+
+        CHECK_FUNCTION(pthread_create(&pthreads[i], NULL, pi_counter, (void*) &thread_info[i]), \
+        destroy_all_elements(mutex, pthreads, thread_info); return EXIT_FAILURE);
     }
 
     double pi_part = 0;
-    double* local_result;
-    for (int i = 0; i < ACTUAL_NUMBER_OF_THREADS; ++i)
+    double* thread_returned;
+    for (int i = 0; i < NUMBER_OF_THREADS; ++i)
     {
-        CHECK_FUNCTION(pthread_join(pthreads[i], (void**)&local_result), free(pthreads); free(displacements); return EXIT_FAILURE);
-        CHECK_THREAD_ARGUMENTS(local_result, free(pthreads); free(displacements); return EXIT_FAILURE);
-        pi_part += displacements[i].local_result;
+        CHECK_FUNCTION(pthread_join(pthreads[i], (void**)&thread_returned), \
+        destroy_all_elements(mutex, pthreads, thread_info); return EXIT_FAILURE);
+
+        CHECK_THREAD_ARGUMENTS(thread_returned, destroy_all_elements(mutex, pthreads, thread_info); return EXIT_FAILURE);
+
+        pi_part += thread_info[i].local_result;
     }
 
-    printf("Pi ~ %.15g\n", pi_part * 4);
+    printf("\n\tPi ~ %.15g\n", pi_part * 4);
 
-    free(pthreads);
-    free(displacements);
+    destroy_all_elements(mutex, pthreads, thread_info);
 
     return EXIT_SUCCESS;
 }
-
 
 void* pi_counter(void* argv)
 {
     CHECK_NULL(argv, return (void*)EXIT_FAILURE);
-	thread_data* const displacement = (thread_data* const) argv;
+	thread_data* const thread_info = (thread_data* const) argv;
 
-    for (long long i = displacement->start; i < displacement->number_of_iterations; ++i)
+    long long current_iteration = 0;
+    for (long long i = thread_info->start; true; i += NUMBER_OF_THREADS)
     {
-        displacement->local_result += 1.0 / (i * 4.0 + 1.0);
-        displacement->local_result -= 1.0 / (i * 4.0 + 3.0);
+        thread_info->local_result += 1.0 / (i * 4.0 + 1.0) - 1.0 / (i * 4.0 + 3.0);
+        ++current_iteration;
+
+        if (current_iteration % CHECK_STEP == 0)
+        {        
+            CHECK_MUTEX(pthread_mutex_lock(&mutex));
+            if (current_iteration > MAX_ITERATION)
+            {
+                MAX_ITERATION = current_iteration;
+            }
+            CHECK_MUTEX(pthread_mutex_unlock(&mutex));
+
+            if (true == exit_flag)
+            {
+                if (current_iteration < MAX_ITERATION)
+                {
+                    printf("\n[tid - %lu] {iter - %lld} is going to count more\n", pthread_self(), current_iteration);
+                    count_to_the_end(i + 1, MAX_ITERATION - current_iteration, &thread_info->local_result);
+                    current_iteration = MAX_ITERATION;
+                }
+                printf("\n[tid - %lu] {iter - %lld} exited\n", pthread_self(), current_iteration);
+                pthread_exit(NULL);
+            }  
+        }
     }
     return NULL;
 }
 
-
-void fill_displacements_data(const int NUMBER_OF_THREADS, thread_data* const displacements)
+void count_to_the_end(long long start_multiplier, long long number_of_iterations, double* result)
 {
-    for (int i = 0; i < NUMBER_OF_THREADS; ++i)
+    long long multiplier = start_multiplier;
+    for (long long i = 0; i < number_of_iterations; ++i)
     {
-        displacements[i].number_of_iterations = NUMBER_OF_STEPS / NUMBER_OF_THREADS;
-    }
-
-    for (int i = 0; i < NUMBER_OF_STEPS % NUMBER_OF_THREADS; ++i)
-    {
-        ++displacements[i].number_of_iterations;
-    }
-
-    int displacement_index = 0;
-    for (int i = 0; i < NUMBER_OF_THREADS; ++i)
-    {
-        displacements[i].start = displacement_index;
-        displacement_index += displacements[i].number_of_iterations; 
+        *result += 1.0 / (multiplier * 4.0 + 1.0) - 1.0 / (multiplier * 4.0 + 3.0);
+        multiplier += NUMBER_OF_THREADS;
     }
 }
 
-int memory_allocation(const int NUMBER_OF_THREADS, pthread_t** pthreads, thread_data** displacements)
+int set_sigint_handler()
+{
+    struct sigaction sigint_handler;
+    memset(&sigint_handler, 0 , sizeof(struct sigaction));
+
+    sigint_handler.sa_handler = signal_handler;
+
+    CHECK_FUNCTION(sigaction(SIGINT, &sigint_handler, NULL), return EXIT_FAILURE);
+
+    return EXIT_SUCCESS;
+}
+
+void signal_handler(int signal)
+{
+    exit_flag = true;
+}
+
+int memory_allocation(pthread_t** pthreads, thread_data** thread_info)
 {
     *pthreads = malloc(sizeof(pthread_t) * NUMBER_OF_THREADS);
     CHECK_NULL(*pthreads, return EXIT_FAILURE);
 
-    *displacements = calloc(NUMBER_OF_THREADS, sizeof(thread_data));
-    CHECK_NULL(*displacements, free(*pthreads); return EXIT_FAILURE);
+    *thread_info = calloc(NUMBER_OF_THREADS, sizeof(thread_data));
+    CHECK_NULL(*thread_info, free(*pthreads); return EXIT_FAILURE);
 
     return EXIT_SUCCESS;
+}
+
+void destroy_all_elements(pthread_mutex_t mutex, pthread_t* pthreads, thread_data* thread_info)
+{
+    pthread_mutex_destroy(&mutex);
+    free(pthreads);
+    free(thread_info);
 }
