bf54e5ad0c3d9d1c142de00dbd20f7b68a18c34a
unknownRobert <daniil.lyutaev@gmail.com>
2022-10-16 23:16:46 +0700

    create wrappers for checking errors

--- src/lab_8/main.c
+++ src/lab_8/main.c
@@ -4,16 +4,41 @@
 #include <pthread.h>
 
 #define NUMBER_OF_STEPS 2000000000
+#define CHECK_FUNCTION(function, action_on_error) do \
+        { \
+            int return_value = (function); \
+            if (EXIT_SUCCESS != return_value) { \
+                fprintf(stderr, "Runtime error: %s returned %d at %s:%d\n", #function, return_value, __FILE__, __LINE__); \
+                action_on_error; \
+            } \
+        } while (0)
+
+#define CHECK_THREAD_ARGUMENTS(return_value, action_on_error) do \
+        { \
+            if ((void*)EXIT_FAILURE == return_value) { \
+                fprintf(stderr, "Runtime error: Thread have got wrong arguments. Error at %s:%d\n", __FILE__, __LINE__); \
+                action_on_error; \
+            } \
+        } while (0)
+
+#define CHECK_NULL(value, action_on_error) do\
+        { \
+            if (NULL == value) { \
+                fprintf(stderr, "Runtime error: Unexpected NULL occured at %s:%d\n", __FILE__, __LINE__); \
+                action_on_error; \
+            } \
+        } while (0)
 
 typedef struct thread_data
 {
     long long start;
     long long number_of_iterations;
+    double local_result;
 } thread_data;
 
 
 void* pi_counter(void* argv);
-void fill_displacements_data(const int NUMBER_OF_THREADS, thread_data* displacements);
+void fill_displacements_data(const int NUMBER_OF_THREADS, thread_data* const displacements);
 int memory_allocation(const int NUMBER_OF_THREADS, pthread_t** pthreads, thread_data** displacements);
 
 int main(int argc, char** argv)
@@ -35,49 +60,27 @@ int main(int argc, char** argv)
 
     pthread_t* pthreads;
     thread_data* displacements;
-    double* pi_partial;
 
-    if (EXIT_FAILURE == memory_allocation(EXPECTED_NUMBER_OF_THREADS, &pthreads, &displacements))
-    {
-        fprintf(stderr, "Memory allocation failed\n");
-        return EXIT_FAILURE;
-    }
+    CHECK_FUNCTION(memory_allocation(EXPECTED_NUMBER_OF_THREADS, &pthreads, &displacements), return EXIT_FAILURE);
 
     fill_displacements_data(EXPECTED_NUMBER_OF_THREADS, displacements);
 
     for (int i = 0; i < EXPECTED_NUMBER_OF_THREADS; ++i)
     {
-        if (0 != pthread_create(&pthreads[i], NULL, pi_counter, (void*) &displacements[i]))
-        {
-            perror("Failed to create a thread");
-            fprintf(stderr, "Number of steps less than required\n");
-            break;
-        }
+        CHECK_FUNCTION(pthread_create(&pthreads[i], NULL, pi_counter, (void*) &displacements[i]), break);
         ++ACTUAL_NUMBER_OF_THREADS;
     }
 
-    double pi_divided_by_4 = 0;
+    double pi_part = 0;
     double* local_result;
     for (int i = 0; i < ACTUAL_NUMBER_OF_THREADS; ++i)
     {
-        if (0 != pthread_join(pthreads[i], (void**)&local_result))
-        {
-            perror("Failed to join the thread");
-            free(pthreads);
-            free(displacements);
-            return EXIT_FAILURE;
-        }
-        if ((void*)EXIT_FAILURE == local_result)
-        {
-            free(pthreads);
-            free(displacements);
-            return EXIT_FAILURE;
-        }
-        pi_divided_by_4 += (*local_result);
-        free(local_result);
+        CHECK_FUNCTION(pthread_join(pthreads[i], (void**)&local_result), free(pthreads); free(displacements); return EXIT_FAILURE);
+        CHECK_THREAD_ARGUMENTS(local_result, free(pthreads); free(displacements); return EXIT_FAILURE);
+        pi_part += displacements[i].local_result;
     }
 
-    printf("Pi ~ %.15g\n", pi_divided_by_4 * 4);
+    printf("Pi ~ %.15g\n", pi_part * 4);
 
     free(pthreads);
     free(displacements);
@@ -88,30 +91,19 @@ int main(int argc, char** argv)
 
 void* pi_counter(void* argv)
 {
-    if (NULL == argv)
-	{
-		fprintf(stderr, "Wrong arguments for 'print_lines()'\n");
-		return (void*)EXIT_FAILURE;
-	}
-	const thread_data* const displacement = (const thread_data* const) argv;
-
-    double* local_result = calloc(1, sizeof(double));
-    if (NULL == local_result)
-    {
-        perror("calloc() failed");
-        return (void*)EXIT_FAILURE;
-    }
+    CHECK_NULL(argv, return (void*)EXIT_FAILURE);
+	thread_data* const displacement = (thread_data* const) argv;
 
     for (long long i = displacement->start; i < displacement->number_of_iterations; ++i)
     {
-        *local_result += 1.0 / (i * 4.0 + 1.0);
-        *local_result -= 1.0 / (i * 4.0 + 3.0);
+        displacement->local_result += 1.0 / (i * 4.0 + 1.0);
+        displacement->local_result -= 1.0 / (i * 4.0 + 3.0);
     }
-    return (void*)local_result;
+    return NULL;
 }
 
 
-void fill_displacements_data(const int NUMBER_OF_THREADS, thread_data* displacements)
+void fill_displacements_data(const int NUMBER_OF_THREADS, thread_data* const displacements)
 {
     for (int i = 0; i < NUMBER_OF_THREADS; ++i)
     {
@@ -133,19 +125,11 @@ void fill_displacements_data(const int NUMBER_OF_THREADS, thread_data* displacem
 
 int memory_allocation(const int NUMBER_OF_THREADS, pthread_t** pthreads, thread_data** displacements)
 {
-    *pthreads = malloc(sizeof(pthreads) * NUMBER_OF_THREADS);
-    if (NULL == *pthreads)
-    {
-        perror("malloc() failed");
-        return EXIT_FAILURE;
-    }
+    *pthreads = malloc(sizeof(pthread_t) * NUMBER_OF_THREADS);
+    CHECK_NULL(*pthreads, return EXIT_FAILURE);
 
-    *displacements = malloc(sizeof(thread_data) * NUMBER_OF_THREADS);
-    if (NULL == *displacements)
-    {
-        perror("malloc() failed");
-        free(*pthreads);
-        return EXIT_FAILURE;
-    }
+    *displacements = calloc(NUMBER_OF_THREADS, sizeof(thread_data));
+    CHECK_NULL(*displacements, free(*pthreads); return EXIT_FAILURE);
+
     return EXIT_SUCCESS;
 }
